3c83430dac337e22e22da9f5ce4d4c7623509260
shinchanhan <90810185+shinchanhan@users.noreply.github.com>
2021-09-16 12:28:03 +1000

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -4,7 +4,7 @@ bin=$HOME/GRACEFO/bin
 gfortran -o $bin/Read_GRFO_L1B.e   $src/Read_GRFO_L1B.f90    $HOME/ftools/src/SBRT.interp1.f90  $HOME/ftools/src/SBRT.SPLINE.f90
 rm *.mod
 
-gfortran -o $bin/Compute_LGD_AOD1B.e  $src/Compute_LGD_AOD1B.f90
+gfortran -o $bin/Compute_LGD.e  $src/Compute_LGD.f90
 
 gfortran -o $bin/rr2lgd.e $src/rr2lgd.f90 $HOME/ftools/src/FFTCC.f $HOME/ftools/src/SBRT.GetFrequency.f90 $HOME/ftools/src/SBRT.gradient.f90 $HOME/ftools/src/SBRT.filter_butterworth.f90
 
